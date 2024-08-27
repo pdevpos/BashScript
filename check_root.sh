e41d647ccbe989efcf1e6a83a4e2d82f3536eae6
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-08-27 19:15:03 +0530

    Install nginx

--- check_root.sh
+++ check_root.sh
@@ -3,11 +3,12 @@
 #userid=$(id)
 #echo "user id: $userid"
 userid=$(id -u)
-if [ $userid -ne 0 ];
+if [ $userid -ne 0 ]
 then
   echo "please run the script with root privileges : $userid"
   exit 1
 fi
+
 dnf list installed nginx
 if [ $? -ne 0 ];
 then
