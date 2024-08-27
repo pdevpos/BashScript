7a15e6b174f05163e543f35cac352a9f45573798
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-08-27 19:00:29 +0530

    run script with root priviliges

--- check_root.sh
+++ check_root.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-id
+#id
+#userid=$(id)
+#echo "user id: $userid"
 userid=$(id)
-echo "user id: $userid"
 if [ $userid -ne 0 ];
 then
-  echo "please run the script with root priviliges"
+  echo "please run the script with root privileges : $userid"
 fi
 dnf install nginx -y
