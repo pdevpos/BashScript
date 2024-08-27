f1d29af10dbe7c74bc87e22033b749289eac9ad0
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-08-27 19:36:02 +0530

    to reuse the code

--- 01-fun.sh
+++ 01-fun.sh
@@ -7,6 +7,22 @@ fi
 validate()
 {
   echo "exist status.. $1"
+  if [ $1 -ne 0 ]; then
+    echo "$2 failed"
+    exit 1
+  else
+    echo "$2 success"
+
+  fi
 }
-validate $?
+
+dnf list installed nginx
+if [ $? -ne 0 ]; then
+  echo "nginx not installed...please install"
+  dnf install nginx -y
+  validate $? "nginx installed"
+
+else
+  echo "nginx already installed"
+fi
 
