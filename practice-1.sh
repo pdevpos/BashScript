b9bce0cb5b0dcf361779e4725432163dc9d210b6
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-08-28 14:14:31 +0530

    Install nginx

--- practice-1.sh
+++ practice-1.sh
@@ -3,7 +3,7 @@ USERID=$(id -u)
 check_root(){
   if [ $USERID -ne 0 ]; then
     echo "execute script in root privileges:: $USERID"
-
+    exit 1
   fi
 }
 check_root
@@ -11,7 +11,7 @@ dnf list installed nginx
 if [ $? -ne 0 ]
 then
   echo "nginx not installed please install...."
-  dnf install nginx
+  dnf install nginx -y
 else
   echo "nginx is installed already...."
 fi
