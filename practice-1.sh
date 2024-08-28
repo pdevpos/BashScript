af992b892a518a50f4d75b7a270ea6fd464b9f7e
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-08-28 14:28:04 +0530

    status display in color

--- practice-1.sh
+++ practice-1.sh
@@ -10,18 +10,29 @@ check_root(){
     exit 1
   fi
 }
+validate()
+{
+  if [ $1 -ne 0 ]; then
+    echo -e "nginx  not installed properly its $R failure.check it..."
+  else
+    echo -e "nginx installed $G success $N."
+
+  fi
+}
 check_root
 dnf list installed nginx
 if [ $? -ne 0 ]
 then
   echo "nginx not installed please install...."
   dnf install nginx -y
-  if [ $? -ne 0 ]; then
-    echo -e "nginx installed $R failure.check it..."
-  else
-    echo -e "nginx installed $G success $N."
-
-  fi
+#  if [ $? -ne 0 ]; then
+#    echo -e "nginx installed $R failure.check it..."
+#  else
+#    echo -e "nginx installed $G success $N."
+#
+#  fi
+   validate $?
 else
   echo -e "$Y nginx is installed already...."
 fi
+
