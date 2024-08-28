de314e95781aba4e4e4f26c666473b03c1c5501f
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-08-28 14:06:17 +0530

    check root user

--- practice-1.sh
+++ practice-1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 USERID=$(id -u)
 check_root(){
-  if [ $1 -ne 0 ]; then
-    echo "execute script in root privileges $USERID"
+  if [ $USERID -ne 0 ]; then
+    echo "execute script in root privileges:: $USERID"
 
   fi
 }
-check_root $?
+check_root
